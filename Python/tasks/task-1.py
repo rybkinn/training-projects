ad24c259a866334ed96c25fb131fd9b99f29e5f1
developer-rni <developer.rni@gmail.com>
2021-03-01 13:25:51 +0300

    solution task-1

--- Python/tasks/task-1.py
+++ Python/tasks/task-1.py
@@ -1,4 +1,20 @@
 """
-Замените пробелы на указанные символы.Напишите метод для замены всех пробелов в строке на '%20'.
+Замените пробелы на указанные символы.
+Напишите метод для замены всех пробелов в строке на '%20'.
 Игнорируйте любые дополнительные пробелы в начале или конце строки.
 """
+
+
+class MyClass:
+    def __init__(self, string=str):
+        self.string = string
+
+    def replacement_space(self):
+        self.string = self.string.strip()
+        self.string = self.string.replace(' ', '%20')
+
+
+p1 = MyClass('  ch e ck   ')
+print(p1.string)
+p1.replacement_space()
+print(p1.string)
